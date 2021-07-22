63293cb2a0d053879e6a31cbdd2835f5dfd5b1d1
shi0ge3 <katsurashigeki0325@gmail.com>
2021-07-22 23:46:07 +0900

    JavaScript学習(Ch7 10/10)
    コールバック関数演習の正解。

--- Lesson/Ch7/call_back_q.js
+++ Lesson/Ch7/call_back_q.js
@@ -1,13 +1,19 @@
 // 問題１
 const person = {
-    hello: () =>  {
+    hello: function () {
         return 'hello Tom';
     }
 }
-console.log(person.hello());
+
+setTimeout(function() {
+    const hello = person.hello();
+    console.log(hello);
+}, 1000);
 
 // 問題２
 setTimeout(person.hello(), 1000);
 
 // 問題３
 // hello:理由 heyはまだ実行に移っていない為。
+
+// 問題４
