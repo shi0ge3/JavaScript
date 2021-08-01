15425a552785b8c4eb19d6363fbb01bf9b75b4de
shi0ge3 <katsurashigeki0325@gmail.com>
2021-08-02 00:20:46 +0900

    CH8 関数とオブジェクト２
    prototype

---
+++ Lesson/Ch8/prototype.js
@@ -0,0 +1,16 @@
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.hello = function() {
+    console.log('hello ' + this.name);
+}
+
+const bob = new Person('Bob', 18);
+const tom = new Person('Tom', 33);
+const sun = new Person('Sun', 20);
+
+// Personのプロトタイプとbob or tomのインスタンスオブジェクトの__proto__は同じ。
+bob.hello();
+tom.hello();
