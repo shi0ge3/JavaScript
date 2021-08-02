63a2fc0973ad9a4642bf88a53ccc559c97ac1430
shi0ge3 <katsurashigeki0325@gmail.com>
2021-08-03 00:29:47 +0900

    CH8 関数とオブジェクト２
    new演算子

--- Lesson/Ch8/new.js
+++ Lesson/Ch8/new.js
@@ -2,8 +2,16 @@
 function F(a, b) {
     this.a = a;
     this.b = b;
-    return 1;
+    return {};
 }
 
-const instance = new F(1, 2);
+// prototypeがある場合。
+F.prototype.c = function() {}
+
+// new演算子の様な関数
+function newOpe(C, ...args) {
+    console.log(args);
+}
+
+const instance = newOpe(F, 1, 2);
 console.log(instance); // プリミティブ型が返される場合。
