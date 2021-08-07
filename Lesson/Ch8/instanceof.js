fab3e1b680428df64e038d56c1a2ffce18ecc4db
shi0ge3 <katsurashigeki0325@gmail.com>
2021-08-07 21:53:02 +0900

    CH8 関数とオブジェクト２
    instanceof

--- Lesson/Ch8/instanceof.js
+++ Lesson/Ch8/instanceof.js
@@ -3,6 +3,8 @@
 function F(a, b) {
     this.a = a;
     this.b = b;
+    const result = new Object();
+    result.a = 1;
     return {a: 1};
 }
 
@@ -11,3 +13,5 @@ F.prototype.c = function() {}
 const instance = new F(1, 2);
 // どのオブジェクトを生成されたのかを確認。
 console.log(instance instanceof F);
+console.log(instance.__proto__ === F.prototype); // falseの理由はObjectクラスのコンストラクタのため。
+console.log(instance.__proto__ === Object.prototype); // Trueが返されている。
