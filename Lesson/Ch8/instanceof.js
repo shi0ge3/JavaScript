1cc68230aeb25de91ebbbd2ffd319d266b6dca45
shi0ge3 <katsurashigeki0325@gmail.com>
2021-08-07 21:59:35 +0900

    CH8 関数とオブジェクト２
    instanceof

--- Lesson/Ch8/instanceof.js
+++ Lesson/Ch8/instanceof.js
@@ -3,9 +3,9 @@
 function F(a, b) {
     this.a = a;
     this.b = b;
-    const result = new Object();
-    result.a = 1;
-    return {a: 1};
+    // const result = new Object(); // ここからreturn文までコメントアウトするとFクラスのインスタンスとなる。
+    // result.a = 1;
+    // return {a: 1};
 }
 
 F.prototype.c = function() {}
@@ -15,3 +15,16 @@ const instance = new F(1, 2);
 console.log(instance instanceof F);
 console.log(instance.__proto__ === F.prototype); // falseの理由はObjectクラスのコンストラクタのため。
 console.log(instance.__proto__ === Object.prototype); // Trueが返されている。
+
+// 引数argにはオブジェクトの配列が渡される想定。
+function fn(arg) {
+    // 作成元のインスタンスに応じて処理分岐を行わせたい。
+    if(arg instanceof Array) {
+        arg.push('value');
+    } else {
+        arg['key'] = value;
+    }
+    console.log(arg);
+}
+
+fn([]);
