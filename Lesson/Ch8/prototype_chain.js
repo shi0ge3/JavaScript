e1399c8c6c05d390a4560ac659782d086ac49f87
shi0ge3 <katsurashigeki0325@gmail.com>
2021-08-10 00:36:05 +0900

    CH8 関数とオブジェクト２
    プロトタイプチェーン

--- Lesson/Ch8/prototype_chain.js
+++ Lesson/Ch8/prototype_chain.js
@@ -1,12 +1,18 @@
+// ProtoTypeChain：階層が浅い順にプロトタイプを参照していくこと。
+// 優先順位：自身のプロパティ→インスタンスのプロトタイプ→オブジェクトプロトタイプ
+
 function Person(name, age) {
     this.name = name;
     this.age = age;
+    this.hello = function() {
+      console.log('OwnProperty: hello ' + this.name);
+    }
   }
 
   // PersonとObjectのプロトタイプチェーンを比較。
-  // Person.prototype.hello = function() {
-  //   console.log('Person: hello ' + this.name);
-  // }
+  Person.prototype.hello = function() {
+    console.log('Person: hello ' + this.name);
+  }
 
   // Personのオブジェクトが無効であるとこちらのObjectになる。
   Object.prototype.hello = function() {
