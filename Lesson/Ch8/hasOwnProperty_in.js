aabb264d0b6f3853862c4e268730a633cb3e13d7
shi0ge3 <katsurashigeki0325@gmail.com>
2021-08-11 00:22:18 +0900

    CH8 関数とオブジェクト２
    hasOwnPropertyとin

--- Lesson/Ch8/hasOwnProperty_in.js
+++ Lesson/Ch8/hasOwnProperty_in.js
@@ -11,4 +11,4 @@ const bob = new Person('Bob', 18);
 const result = bob.hasOwnProperty('hello');
 console.log(result);
 // in関数は、プロトタイプチェーンを遡って探す。
-console.log('name' in bob);
+console.log('hasOwnProperty' in bob);
