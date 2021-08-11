52d4d545dab61fae906b6b0c48dc14bd7b0901cf
shi0ge3 <katsurashigeki0325@gmail.com>
2021-08-12 00:33:38 +0900

    CH8 関数とオブジェクト２
    プロトタイプ継承

--- Lesson/Ch8/prototype_inheritance.js
+++ Lesson/Ch8/prototype_inheritance.js
@@ -10,8 +10,13 @@ function Person(name, age) {
     console.log('hello ' + this.name);
   }
 
+  // 継承の例
   function Japanese(name, age) {
-
+    Person.call(this, name, age);
   }
   // プロトタイプ継承の例
   Japanese.prototype = Object.create(Person.prototype);
+
+  const taro = new Japanese('Taro', 23);
+  console.log(taro);
+  taro.hello(); // Personのhello関数が呼び出し可能。
