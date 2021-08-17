aff47afcbf0f39bb5fa47ee2531e9ddb9c0b0f9e
shi0ge3 <katsurashigeki0325@gmail.com>
2021-08-18 00:11:08 +0900

    CH8 関数とオブジェクト２
    クラスの継承

---
+++ Lesson/Ch8/class_inheritance.js
@@ -0,0 +1,52 @@
+// クラス継承：他のクラスのプロパティーとメソッドを継承すること。
+class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+
+    hello() {
+      console.log('hello ' + this.name);
+    }
+  }
+
+  // function Person(name, age) {
+  //   this.name = name;
+  //   this.age = age;
+  // }
+
+  // Person.prototype.hello = function() {
+  //   console.log('hello ' + this.name);
+  // }
+
+  class Japanese extends Person {
+    constructor(name, age, gender) {
+      super(name, age);
+      this.gender = gender;
+    }
+
+    hello() {
+      console.log('Konnichiwa ' + this.name);
+    }
+
+    bye() {
+      console.log('Sayonara ' + this.name);
+    }
+  }
+
+  // function Japanese(name, age, gender) {
+  //   Person.call(this, name, age);
+  //   this.gender = gender;
+  // }
+
+  // Japanese.prototype = Object.create(Person.prototype);
+
+  // Japanese.prototype.hello = function() {
+  //   console.log('Konnichiwa ' + this.name);
+  // }
+
+  // Japanese.prototype.bye = function() {
+  //   console.log('Sayonara ' + this.name);
+  // }
+
+  const taro = new Japanese('Taro', 23, 'Male');
