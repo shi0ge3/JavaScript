19437bb7d15ec80295df23342802fb5a972681fe
shi0ge3 <katsurashigeki0325@gmail.com>
2021-08-26 01:05:31 +0900

    CH8 関数とオブジェクト２
    Symbol

--- Lesson/Ch8/symbol.js
+++ Lesson/Ch8/symbol.js
@@ -3,3 +3,15 @@ const s = Symbol('hello'); //プリミティブ型であるSymbol
 const s2 = Symbol('hello');
 console.log(s === s2); // sとs2は違う＝違うアドレスに格納されている値
 console.log(typeof s); // symbol型のプリミティブ型とわかる。
+
+const str = new String('Tom');
+console.log(str);
+
+String.prototype[s] = function() {
+
+}
+
+String.prototype[s2] = function() {
+
+}
+// 上記の場合は、プロパティの競合を防ぐためにES６から独立するようになった。
