565f7f50646ee8def94ba0ade74026232efeecdb
shi0ge3 <katsurashigeki0325@gmail.com>
2021-08-28 00:47:18 +0900

    CH8 関数とオブジェクト２
    プロパティとディスクリプター

--- Lesson/Ch8/discripter.js
+++ Lesson/Ch8/discripter.js
@@ -1,6 +1,11 @@
 // ディスクリプター:下記、４つのプロパティーのこと。
 // value:値/configurabel:設定変更可能性/enumerable:列挙可能性/writable:値の変更可能性
 // const obj = {prop: 0};
+const obj = {};
+
+Object.defineProperty(obj, 'prop', {
+    value: 0 // ディスクリプターに値を入れると他のディスクリプターはfalseになる。
+})// オブジェクトにdescriptorを入れる。
 
 const descriptor = Object.getOwnPropertyDescriptor(obj, 'prop');
 
